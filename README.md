20bb2d759c5c9f18cb5787dd4f68d98f06efa6c2
Zahniar Adirahman <zahniar88@gmail.com>
2021-01-10 22:52:20 +0700

    Validator codeigniter

---
+++ README.md
@@ -0,0 +1,97 @@
+# Helper Validator
+Helper validator dibuat untuk membantu proses validasi form pada codeigniter. Memiliki beberapa fitur validasi yang dapat digunakan. Terinspirasi dari validator laravel.
+
+## Cara install
+* Download file Validator_helper
+* letakan pada folder helper codeigniter
+* load file validator dengan cara
+```php
+$this->load->helper("validator_helper");
+```
+
+## Cara menggunakan
+```php
+$this->load->helper("validator_helper");
+$validator = new Validator_helper;
+
+$errors = $validator->setField("username", ["required", "max:30", "alpha_numeric"])
+                    ->setField("password", ["required", "min:8", "confirm", "password"])
+                    ->getErrors();
+```
+
+Jika terjadi error makan ```$errors``` akan menghasilkan output array seperti berikut:
+```php
+array(
+    "username" => "Pesan kesalahan sesuai validasi",
+    "password" => "Pesan kesalahan sesuai validasi"
+);
+```
+
+peraturan penulisan pada ```setField``` seperti berikut:
+```php
+setField("nama_field", ["aturan"], "method");
+// method: post || get -> default: post
+```
+
+## Fungsi yang tersedia
+* Required
+```php
+setField("nama_field", ["aturan"], "method");
+```
+
+* Min panjang karakter
+```php
+setField("nama_field", ["min:angka"], "method");
+```
+
+* Max panjang karakter
+```php
+setField("nama_field", ["max:angka"], "method");
+```
+
+* Alpha Space hanya membolehkan huruf dan spasi
+```php
+setField("nama_field", ["alpha_space"], "method");
+```
+
+* Alpha numeric hanya untuk huruf dan angka
+```php
+setField("nama_field", ["alpha_space"], "method");
+```
+
+* Numeric hanya untuk angka
+```php
+setField("nama_field", ["alpha_space"], "method");
+```
+
+* Regex membuat aturan regex sendiri menggunakan preg_match
+```php
+setField("nama_field", ["regex:([a-z]+)"], "method");
+```
+
+* Db exists, data harus tersedia pada database
+```php
+setField("nama_field", ["db_exists:table,column"], "method");
+```
+
+* Db unique, data harus belum tersedia pada database
+```php
+setField("nama_field", ["db_unique:table,column,except,except_value"], "method");
+```
+
+* In, data harus sama dengan yang disediakan
+```php
+setField("nama_field", ["in:opsi1,opsi2"], "method");
+```
+
+* Confirm, menyamakan field dengan field_confirm
+```php
+setField("nama_field", ["confirm"], "method");
+```
+
+* Password, validasi password harus memiliki huruf besar, kecil dan angka minimal 1
+```php
+setField("nama_field", ["password"], "method");
+```
+
+Untuk sementera baru itu yang dapat saya buat. belum sesempuran milik laravel :)
---
+++ Validator_helper.php
@@ -0,0 +1,174 @@
+<?php 
+
+class Validator_helper
+{
+    protected $fieldDefault;
+    protected $fieldName;
+    protected $fieldValue;
+    protected $method;
+    protected $errors = [];
+
+    public function __construct() {
+        $this->CI =& get_instance();
+    }
+
+    // set field yang akan di validasi
+    public function setField($field, $rules = [], $method = "post")
+    {
+        $this->fieldValue = $this->CI->input->$method($field);
+        $this->fieldName = str_replace("_", " ", ucfirst($field));
+        $this->fieldDefault = $field;
+        $this->method = $method;
+
+        foreach ($rules as $rule) {
+            $data   = explode(":", $rule);
+            $func   = $data[0];
+            $value  = $data[1] ?? "";
+
+            $this->$func($value);
+        }
+
+        return $this;
+    }
+
+    // validasi required
+    protected function required()
+    {
+        if ( strlen($this->fieldValue) == 0 ) {
+            $this->setErrors("$this->fieldName wajib diisi");
+        }
+    }
+
+    // minimal karakter
+    protected function min($val)
+    {
+        if ( strlen($this->fieldValue) < $val ) {
+            $this->setErrors("$this->fieldName  minimal $val karakter");
+        }
+    }
+    
+    // maksimal karakter
+    protected function max($val)
+    {
+        if ( strlen($this->fieldValue) > $val ) {
+            $this->setErrors("$this->fieldName  maksimal $val karakter");
+        }
+    }
+
+    // hanya huruf dan spasi
+    protected function alpha_space()
+    {
+        if ( !preg_match("/(^[a-zA-Z ]+$)/", $this->fieldValue) ) {
+            $this->setErrors("$this->fieldName hanya berupa huruf dan spasi");
+        }
+    }
+    
+    // hanya huruf dan angka
+    protected function alpha_numeric()
+    {
+        if ( !preg_match("/(^[a-zA-Z0-9]+$)/", $this->fieldValue) ) {
+            $this->setErrors("$this->fieldName hanya berupa huruf dan angka");
+        }
+    }
+    
+    // hanya angka
+    protected function numeric()
+    {
+        if ( !preg_match("/(^[0-9]+$)/", $this->fieldValue) ) {
+            $this->setErrors("$this->fieldName hanya berupa angka");
+        }
+    }
+    
+    // regex menggunakan aturan sendiri
+    protected function regex($val)
+    {
+        if ( !preg_match("/$val/", $this->fieldValue) ) {
+            $this->setErrors("$this->fieldName tidak valid");
+        }
+    }
+    
+    // exists pada database
+    protected function db_exists($val)
+    {
+        $data = explode(",", $val);
+        $table     = $data[0];
+        $column     = $data[1];
+
+        $query = "
+        SELECT COUNT(*) AS count FROM $table WHERE $column = '$this->fieldValue'
+        ";
+        $result = $this->CI->db->query($query);
+        $valid = $result->row();
+
+        if ( $valid->count < 1 ) {
+            $this->setErrors("$this->fieldName tidak ada pada database");
+        }
+    }
+    
+    // unique pada database
+    protected function db_unique($val)
+    {
+        $data = explode(",", $val);
+        $table      = $data[0];
+        $column     = $data[1];
+        $except     = $data[2] ?? "";
+        $exceptVal  = $data[3] ?? "";
+
+        $query = "
+        SELECT * FROM $table WHERE $column = '$this->fieldValue'
+        ";
+
+        $result = $this->CI->db->query($query);
+        $rows = $result->row();
+
+        if ( $rows && $exceptVal != $rows->$except ) {
+            $this->setErrors("$this->fieldName telah digunakan sebelumnya");
+        }
+    }
+
+    // harus sama dengan data yang ada
+    protected function in($val)
+    {
+        $data = explode(",", $val);
+        if ( !in_array($this->fieldValue, $data) ) {
+            $this->setErrors("$this->fieldName tidak valid");
+        }
+    }
+
+    // validasi konfirmasi
+    protected function confirm()
+    {
+        $method = $this->method;
+        $confirm = $this->CI->input->$method($this->fieldDefault . "_confirm");
+
+        if ( $this->fieldValue != $confirm ) {
+            $this->setErrors("Konfirmasi ".strtolower($this->fieldName)." tidak sama");
+        }
+    }
+    
+    // validasi password
+    protected function password()
+    {
+        if ( !preg_match("/([a-z]+)/", $this->fieldValue) ) {
+            $this->setErrors("$this->fieldName setidak nya memiliki 1 huruf kecil");
+        } elseif ( !preg_match("/([A-Z]+)/", $this->fieldValue) ) {
+            $this->setErrors("$this->fieldName setidak nya memiliki 1 huruf kapital");
+        } elseif ( !preg_match("/([0-9]+)/", $this->fieldValue) ) {
+            $this->setErrors("$this->fieldName setidak nya memiliki 1 huruf angka");
+        }
+    }
+
+    // mengatur error
+    protected function setErrors($message)
+    {
+        if ( !array_key_exists($this->fieldDefault, $this->errors) ) {
+            $this->errors[$this->fieldDefault] = $message;
+        }
+    }
+
+    // mengambil data error
+    public function getErrors()
+    {
+        return $this->errors;
+    }
+}
